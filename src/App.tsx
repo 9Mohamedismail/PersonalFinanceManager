a8715e3c78e2703aa5522daddaf6efddd41410d3
9Mohamedismail <9Mohamedismail@gmail.com>
2025-07-26 21:23:56 -0400

    dashboard bento grid template

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import BentoGrid from "./components/BentoGrid";
 import SideBar from "./components/SideBar";
 import TopBar from "./components/TopBar";
 
 function App() {
   return (
-    <div className="flex min-h-screen ">
+    <div className="flex min-h-screen">
       <SideBar />
-
       <div className="flex flex-col flex-1 bg-background">
         <TopBar />
-        <div className="h-16 flex items-center justify-between flex-col border text-text gap-y-4 px-6 py-12 bg-white">
-          <p> Main Content </p>
+        <div className="flex-1">
+          <BentoGrid />
         </div>
       </div>
     </div>
---
+++ src/components/BentoGrid.tsx
@@ -0,0 +1,37 @@
+export default function BentoGrid() {
+  return (
+    <div className="bg-gray-50 py-10 px-4 lg:px-8">
+      <div className=" grid lg:grid-cols-3 lg:grid-rows-2 gap-4">
+        {/* Card 1 */}
+        <div className="bg-amber-300  rounded shadow p-6 lg:col-start-1 lg:row-start-1">
+          <p className="text-lg font-semibold text-gray-900">Card 1</p>
+        </div>
+
+        {/* Card 2 */}
+        <div className="bg-amber-300  rounded shadow p-6 lg:col-start-2 lg:row-start-1">
+          <p className="text-lg font-semibold text-gray-900">2</p>
+        </div>
+
+        {/* Card 4 (Wide) */}
+        <div className="bg-amber-300 rounded shadow p-6 lg:col-start-1 lg:row-start-2 lg:col-span-2">
+          <p className="text-lg font-semibold text-gray-900">Card 4 (Wide)</p>
+        </div>
+
+        {/* Column 3 wrapper (spans both rows) */}
+        <div className="lg:col-start-3 lg:row-start-1 lg:row-span-2 flex flex-col gap-4">
+          {/* Card 3 (Tall, flexible) */}
+          <div className="bg-amber-300 rounded shadow p-6 lg:flex-1">
+            <p className="text-lg font-semibold text-gray-900">Card 3 (Tall)</p>
+          </div>
+
+          {/* Card 5 (Short, fixed height) */}
+          <div className="bg-amber-300 rounded shadow p-6 h-[225px]">
+            <p className="text-lg font-semibold text-gray-900">
+              Card 5 (Short)
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
